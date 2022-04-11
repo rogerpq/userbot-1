7b0b5eb64de6898c5090db2ccbf8d0839ebbc883
COBRA <103384166+COBRA1262@users.noreply.github.com>
2022-04-11 05:23:36 +0300

    Update اوامري.py

--- userbot/plugins/اوامري.py
+++ userbot/plugins/اوامري.py
@@ -3,14 +3,14 @@ import re
 from telethon import Button, events
 from telethon.events import CallbackQuery
 
-from VFF35.razan.resources.assistant import *
-from VFF35.razan.resources.mybot import *
-from VFF34 import VFF35
+from Jmthon.razan.resources.assistant import *
+from Jmthon.razan.resources.mybot import *
+from userbot import jmthon
 from ..core import check_owner
 from ..Config import Config
 
-ROZ_IC = "https://telegra.ph/file/4cbb70d2cbcef89687188.jpg"
-ROE = "** هـذه هي قائمة اوامـر سـورس كوبرا **"
+ROZ_IC = "https://telegra.ph/file/51ceb227677df6f830aa7.jpg"
+ROE = "** هـذه هي قائمة اوامـر سـورس كوبرا**"
 
 if Config.TG_BOT_USERNAME is not None and tgbot is not None:
 
@@ -22,7 +22,7 @@ if Config.TG_BOT_USERNAME is not None and tgbot is not None:
         await bot.get_me()
         if query.startswith("اوامري") and event.query.user_id == bot.uid:
             buttons = [
-                [Button.inline("⌯ اوامر الادمن ⌯", data="VFF350")],
+                [Button.inline("⌯ اوامر الادمن ⌯", data="jmthon0")],
                 [
                     Button.inline("⌯ اوامر البوت ⌯", data="rozbot"),
                     Button.inline("⌯ الحساب ⌯", data="Jmrz"),
@@ -54,7 +54,7 @@ if Config.TG_BOT_USERNAME is not None and tgbot is not None:
                 ],
             ]
             result = builder.article(
-                    title="VFF35 - VFF34",
+                    title="JMTHON - USERBOT",
                     text=ROE,
                     buttons=buttons,
                     link_preview=False,
@@ -74,91 +74,91 @@ async def repo(event):
     await event.delete()
 
 
-@VFF35.tgbot.on(CallbackQuery(data=re.compile(rb"VFF350")))
+@jmthon.tgbot.on(CallbackQuery(data=re.compile(rb"jmthon0")))
 @check_owner
 async def _(event):
     buttons = [[Button.inline("التالي", data="jrzst"),]]
     await event.edit(ROZADM, buttons=buttons)
 
-@VFF35.tgbot.on(CallbackQuery(data=re.compile(rb"jrzst")))
+@jmthon.tgbot.on(CallbackQuery(data=re.compile(rb"jrzst")))
 @check_owner
 async def _(event):
     butze = [[Button.inline("التالي", data="tslrzj"),]]
     await event.edit(GRTSTI, buttons=butze)
 
 
-@VFF35.tgbot.on(CallbackQuery(data=re.compile(rb"tslrzj")))
+@jmthon.tgbot.on(CallbackQuery(data=re.compile(rb"tslrzj")))
 @check_owner
 async def _(event):
     buttons = [[Button.inline("التالي", data="krrznd"),]]
     await event.edit(JMAN, buttons=buttons)
 
 
-@VFF35.tgbot.on(CallbackQuery(data=re.compile(rb"krrznd")))
+@jmthon.tgbot.on(CallbackQuery(data=re.compile(rb"krrznd")))
 @check_owner
 async def _(event):
     buttons = [[Button.inline("التالي", data="rozbot"),]]
     await event.edit(TKPRZ, buttons=buttons)
 
 
-@VFF35.tgbot.on(CallbackQuery(data=re.compile(rb"rozbot")))
+@jmthon.tgbot.on(CallbackQuery(data=re.compile(rb"rozbot")))
 @check_owner
 async def _(event):
     buttons = [[Button.inline("التالي", data="Jmrz"),]]
     await event.edit(ROZBOT, buttons=buttons)
 
 
-@VFF35.tgbot.on(CallbackQuery(data=re.compile(rb"Jmrz")))
+@jmthon.tgbot.on(CallbackQuery(data=re.compile(rb"Jmrz")))
 @check_owner
 async def _(event):
     buttons = [[Button.inline("التالي", data="r7brz"),]]
     await event.edit(JROZT, buttons=buttons)
 
 
-@VFF35.tgbot.on(CallbackQuery(data=re.compile(rb"r7brz")))
+@jmthon.tgbot.on(CallbackQuery(data=re.compile(rb"r7brz")))
 @check_owner
 async def _(event):
     buttons = [[Button.inline("التالي", data="sejrz"),]]
     await event.edit(JMTRD, buttons=buttons)
 
 
-@VFF35.tgbot.on(CallbackQuery(data=re.compile(rb"sejrz")))
+@jmthon.tgbot.on(CallbackQuery(data=re.compile(rb"sejrz")))
 @check_owner
 async def _(event):
     buttons = [[Button.inline("التالي", data="gro"),]]
     await event.edit(ROZSEG, buttons=buttons)
 
 
-@VFF35.tgbot.on(CallbackQuery(data=re.compile(rb"gro")))
+@jmthon.tgbot.on(CallbackQuery(data=re.compile(rb"gro")))
 @check_owner
 async def _(event):
     buttons = [[Button.inline("التالي", data="grrz"),]]
     await event.edit(JMGR1,buttons=buttons)
 
 
-@VFF35.tgbot.on(CallbackQuery(data=re.compile(rb"grrz")))
+@jmthon.tgbot.on(CallbackQuery(data=re.compile(rb"grrz")))
 @check_owner
 async def _(event):
     buttons = [[Button.inline("التالي", data="iiers"),]]
     await event.edit(ROZPRV, buttons=buttons)
 
 
-@VFF35.tgbot.on(CallbackQuery(data=re.compile(rb"iiers")))
+@jmthon.tgbot.on(CallbackQuery(data=re.compile(rb"iiers")))
 @check_owner
 async def _(event):
     buttons = [[Button.inline("التالي", data="rfhrz"),]]
     await event.edit(HERP, buttons=buttons)
 
 
-@VFF35.tgbot.on(CallbackQuery(data=re.compile(rb"rfhrz")))
+@jmthon.tgbot.on(CallbackQuery(data=re.compile(rb"rfhrz")))
 @check_owner
 async def _(event):
     buttons = [[Button.inline("التالي", data="uscuxrz"),]]
     await event.edit(T7SHIZ, buttons=buttons)
 
 
-@VFF35.tgbot.on(CallbackQuery(data=re.compile(rb"uscuxrz")))
+@jmthon.tgbot.on(CallbackQuery(data=re.compile(rb"uscuxrz")))
 @check_owner
 async def _(event):
-    buttons = [[Button.inline("رجوع", data="VFF350"),]]
+    buttons = [[Button.inline("رجوع", data="jmthon0"),]]
     await event.edit(CLORN, buttons=buttons)
